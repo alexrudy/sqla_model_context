cecb6147131d8727599c890ff53bb61153823492
Alex Rudy <opensource@alexrudy.net>
2021-04-03 22:55:14 -0700

    Bump version 0.2.2
    - Include py.typed marker
    - Fix readme content type
    - Remove bump2version config from tooling

--- src/sqla_model_context/__init__.py
+++ src/sqla_model_context/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Alex Rudy"""
 __email__ = "opensource@alexrudy.net"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .context import ModelRelativeContextManager, EnforceMode
 
